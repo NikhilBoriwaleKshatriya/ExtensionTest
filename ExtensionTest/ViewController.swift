e58d0c811ea3d052b047145cd1d896dc92fb4bf8
NikhilBoriwaleKshatriya <38933949+NikhilBoriwaleKshatriya@users.noreply.github.com>
2018-05-11 14:49:21 +0530

    Extesnion Swift
    Extesnion Swift

--- ExtensionTest/ViewController.swift
+++ ExtensionTest/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var conformpassword: UITextField!
+    @IBOutlet weak var gmail: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       self.name.applyPadding()
+        self.password.applyPadding()
+        self.conformpassword.applyPadding()
+        self.gmail.applyPadding()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+        
     }
+    
 
-
+}
+extension UITextField
+{
+    func applyPadding()
+         {
+         let Paddingview = UIView(frame:CGRect(x: 0, y: 0, width: 10, height: 30))
+            self.leftView = Paddingview
+            self.leftViewMode = UITextFieldViewMode.always
+    }
+   
 }
 
